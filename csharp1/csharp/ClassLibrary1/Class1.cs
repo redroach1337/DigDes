7aac8ee0bae0e39a92e269810409966f90aafaa7
redroach1337 <48132327+redroach1337@users.noreply.github.com>
2021-03-16 13:19:35 +0300

    Add files via upload

---
+++ csharp1/csharp/ClassLibrary1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class _MyClass_
+    {
+
+        private Dictionary<string, int> Read(string _String_)
+        {
+            
+
+                Dictionary<string, int> _Dictionary_ = new Dictionary<string, int>();
+                var Words_in_text = _String_.Split(' ', '-', ':', ';', '.', ',', '"', '\'', '!', '?', '\n', ',', '[', ']', '(', ')', '{', '}', '»', '«', '…').Where(string_in_processing => !string.IsNullOrEmpty(string_in_processing));
+                var uniqueWords = Words_in_text.Select(string_in_processing => string_in_processing.ToLower().Trim()).Distinct();
+                foreach (var m in uniqueWords)
+                {
+                    _Dictionary_.Add(m, Words_in_text.Count(string_in_processing => string_in_processing.ToLower().Equals(m)));
+                }
+                _Dictionary_ = _Dictionary_.OrderByDescending(string_in_processing => string_in_processing.Value).ToList().ToDictionary(key => key.Key, value => value.Value);
+            return _Dictionary_;
+            
+            
+        }
+
+    }
+    public class MyClass
+    {
+
+        public Dictionary<string, int> Read(string _String_)
+        {
+            var Words_in_text = _String_.Split(' ', '-', ':', ';', '.', ',', '"', '\'', '!', '?', '\n', ',', '[', ']', '(', ')', '{', '}', '»', '«', '…').Where(string_in_processing => !string.IsNullOrEmpty(string_in_processing));
+            var uniqueWords = Words_in_text.Select(string_in_processing => string_in_processing.ToLower().Trim()).Distinct();
+            var __Dictionary__ = new Dictionary<string, int>();
+            
+            Parallel.ForEach(uniqueWords, word => {
+                __Dictionary__.Add(word, Words_in_text.Count(string_in_processing => string_in_processing.ToLower().Equals(word)));
+            });
+
+            __Dictionary__ = __Dictionary__.OrderByDescending(string_in_processing => string_in_processing.Value).ToList().ToDictionary(key => key.Key, value => value.Value);
+
+            return (__Dictionary__);
+        }
+    }
+}
---
+++ csharp1/csharp/csharp/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+using System.Text;
+using System.Collections.Generic;
+using ClassLibrary1;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Parallel()
+        {
+            DateTime StartTime = DateTime.Now;
+            
+            Console.WriteLine("Параллельность");
+
+                string Book = File.ReadAllText("pushkin_a_s-text_0170.txt");
+            
+            MyClass _Exemp_ = new MyClass();
+
+            Dictionary<string, int> _MyDict_;
+
+            _MyDict_ = _Exemp_.Read(Book);
+            StreamWriter SW = new StreamWriter(new FileStream("PARALLEL.txt", FileMode.Create, FileAccess.Write));
+            foreach (var word in _MyDict_)
+            {
+                SW.Write($" {word.Key} {word.Value}" + ";\n");
+            }
+            SW.Close();
+            TimeSpan ts = DateTime.Now.Subtract(StartTime);
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds,
+                    ts.Milliseconds / 10);
+            Console.WriteLine(elapsedTime, "RunTime");
+
+            DateTime NeStartTime = DateTime.Now;
+
+        }
+        static void Unparallel()
+        {
+            DateTime StartTime = DateTime.Now;
+            Console.WriteLine("Непараллельность");
+
+
+
+        string Book = File.ReadAllText("pushkin_a_s-text_0170.txt");
+            _MyClass_ Exam = new _MyClass_();
+
+            var _type_ = Exam.GetType();
+            
+            var WordsIn = (Dictionary<string, int>)_type_.GetMethod("Read", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(Exam, new object[] { Book });
+            StreamWriter SW = new StreamWriter(new FileStream("NOPARRALEL.txt", FileMode.Create, FileAccess.Write));
+            foreach (var word in WordsIn)
+            {
+                SW.Write($" {word.Key} {word.Value}" + ";\n");
+            }
+
+
+            SW.Close();
+
+            TimeSpan ts = DateTime.Now.Subtract(StartTime);
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds,
+                    ts.Milliseconds / 10);
+            Console.WriteLine(elapsedTime, "RunTime");
+
+            DateTime NeStartTime = DateTime.Now;
+
+        }
+            static void Main(string[] args)
+        {
+            Parallel();
+            Unparallel();
+            Console.ReadLine();
+        }
+    }
+}
+
+
